13e20c2180e988742759f5f82e636665e4bcdecf
prashant Maurya <prashantmaurya1009@gmail.com>
2025-08-10 22:44:53 +0530

    test suit init

--- test_sockjs_project/src/App.jsx
+++ test_sockjs_project/src/App.jsx
@@ -1,62 +1,19 @@
-import { useState } from "react";
+import React from "react";
+import WebSocketEndPointSuit from "./suits/websocket-endpoint-suit";
+
+/**
+ * @param {React.PropsWithChildren}
+ */
+function Container({ children }) {
+  return <div className="w-[90%] h-max">{children}</div>;
+}
 
 function App() {
   return (
-    <div
-      style={{
-        width: "80vw",
-        height: "50vh",
-      }}
-    >
-      <span
-        id="screen"
-        style={{
-          display: "block",
-          outline: "1px solid red",
-          background: "black",
-          width: "100%",
-          height: "50%",
-          fontSize: "17px",
-          textWrap: "wrap",
-        }}
-      ></span>
-      <button
-        onClick={async () => {
-          const res = await fetch("http://localhost:8080/auth/login", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              password: "prashant",
-              username: "prashantmaurya109@gmail.com",
-            }),
-          });
-
-          document.getElementById("screen").innerText = JSON.stringify(
-            await res.json(),
-            null,
-            "   ",
-          );
-        }}
-      >
-        get token
-      </button>
-      <button
-        onClick={async () => {
-          const res = await fetch("http://localhost:8080/auth/user", {
-            method: "GET",
-          });
-
-          document.getElementById("screen").innerText = JSON.stringify(
-            await res.json(),
-            null,
-            "   ",
-          );
-        }}
-      >
-        Hit Me!
-      </button>
+    <div className="w-full h-max flex justify-center bg-bg ">
+      <Container>
+        <WebSocketEndPointSuit />
+      </Container>
     </div>
   );
 }
